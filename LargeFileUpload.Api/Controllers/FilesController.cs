a9a7cecc6baff8e3a9aa266ea29fe472fb08b1cb
Wakil Hasan <whasan4@dxc.com>
2020-05-04 12:07:55 +1000

    initial commit

---
+++ LargeFileUpload.Api/Controllers/FilesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace LargeFileUpload.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public FilesController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+        // GET: api/Files
+        [HttpGet]
+        public async Task<bool> GetToken(string tokenId)
+        {
+            return true;
+        }
+
+        // POST: api/Files
+        [HttpPost("UploadFile")]
+        [DisableRequestSizeLimit]
+        [RequestFormLimits(MultipartBodyLengthLimit = 4097152000)]
+        [RequestSizeLimit(4097152000)]
+        public async Task<IActionResult> UploadFile()
+        {
+            var file = Request.Form.Files[0];
+            
+            CloudStorageAccount storageAccount = null;
+            CloudBlobContainer cloudBlobContainer = null;
+            string storageConnectionString = _configuration["storageconnectionstring"];
+            // OPTION B: read directly from stream for blob upload      
+            
+            try
+            {
+                // Define the BlobRequestOptions on the upload.
+                // This includes defining an exponential retry policy to ensure that failed connections are retried with a backoff policy. As multiple large files are being uploaded
+                // large block sizes this can cause an issue if an exponential retry policy is not defined.  Additionally parallel operations are enabled with a thread count of 8
+                // This could be should be multiple of the number of cores that the machine has. Lastly MD5 hash validation is disabled for this example, this improves the upload speed.
+                BlobRequestOptions options = new BlobRequestOptions
+                {
+                    ParallelOperationThreadCount = 8,
+                    DisableContentMD5Validation = true,
+                    StoreBlobContentMD5 = false
+                };
+                // Check whether the connection string can be parsed.
+                if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount)) 
+                {
+                    // Create the CloudBlobClient that represents the Blob storage endpoint for the storage account.
+                    CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                    // get container called 'apvma-largefile' . 
+                    cloudBlobContainer = cloudBlobClient.GetContainerReference("apvma-largeblob");
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(file.FileName);
+                    // Set block size to 100MB.
+                    cloudBlockBlob.StreamWriteSizeInBytes = 100 * 1024 * 1024;
+
+                    //upload the file to blob.
+                    using (var fileStream = file.OpenReadStream())
+                    {
+                        await cloudBlockBlob.UploadFromStreamAsync(fileStream, null, options, null);
+                    }
+                }
+                return Ok();
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
---
+++ LargeFileUpload.Api/Services/AzureService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+
+namespace LargeFileUpload.Api.Services
+{
+    public class AzureService: IAzureService
+    {
+        private IConfiguration _configuration;
+        public AzureService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string CreateTokenByCustomerId(string id)
+        {
+            var containerName = "apvma-largeblob";
+            string storageConnectionString = _configuration["storageconnectionstring"];
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            var writeOnlyPolicy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessStartTime = DateTime.Now,
+                SharedAccessExpiryTime = DateTime.Now.AddHours(2),
+                Permissions = SharedAccessBlobPermissions.Write
+            };
+
+            var sas = container.GetSharedAccessSignature(writeOnlyPolicy);
+            return sas;
+        }
+    }
+}
